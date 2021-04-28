3dff834eaaa65afadfe6f84e68af5db7bf6719b7
EunJin Lee <didvud0323@gmail.com>
2021-04-28 18:33:03 +0900

    themeforest 템플릿 셋팅작업(static 설정, extends 적용, 화면 레이아웃 잡기)

--- architong/config/urls.py
+++ architong/config/urls.py
@@ -18,11 +18,14 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 from apps.common.views import *
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="index.html")),
     path('accounts/', include('allauth.urls')),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('edit/', editor),
     path('view/', viewer),
     path('martor/', include('martor.urls')),
